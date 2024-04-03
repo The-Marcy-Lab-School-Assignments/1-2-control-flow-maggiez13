b2d1669c0e48095b4f9e552de5940a14c89ad012
Mei Zhu <maggiez0613@gmail.com>
2024-04-03 13:13:11 -0400

    q1 q2 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,10 +1,60 @@
-const measureRain = () => {
+// need to fix
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log("drought");
+  } else if (inches < 2) {
+    console.log("dry");
+  } else if (inches < 4) {
+    console.log("average");
+  } else if (inches < 6) {
+    console.log("rainy");
+  } else {
+    console.log("flood");
+  }
 };
 
-const happyBirthdayPet = () => {
+// (measureRain(0));
+// (measureRain(1));
+// (measureRain(2));
+// (measureRain(3));
+// (measureRain(4));
+// (measureRain(5));
+// (measureRain(6));
+// (measureRain(7));
+// (measureRain(10));
+
+
+const happyBirthdayPet = (breed, age) => {
+  if (breed === "snake") {
+    console.log("Hiss hiss!");
+  } else if (breed === "cat" && age < 5) {
+    console.log("Mew mew!");
+  } else if (breed === "cat" && age  >= 5) {
+    console.log("Meow meow!");
+  } else if (breed === "dog" && age < 5) {
+    console.log("Arf arf!");
+  } else if (breed === "dog" && age >= 5 && age < 10) {
+    console.log("Woof woof!");
+  } else if (breed === "dog" && age >= 10) {
+    console.log("Boof!");
+  } else {
+    console.log("Happy birthday!");
+  }
 };
 
-const funTypes = () => {
+// console.log(happyBirthdayPet("snake", 5));
+// console.log(happyBirthdayPet("cat", 3));
+// console.log(happyBirthdayPet("cat", 5));
+// console.log(happyBirthdayPet("cat", 7));
+// console.log(happyBirthdayPet("dog", 3));
+// console.log(happyBirthdayPet("dog", 5));
+// console.log(happyBirthdayPet("dog", 9));
+// console.log(happyBirthdayPet("dog", 10));
+// console.log(happyBirthdayPet("dog", 12));
+// console.log(happyBirthdayPet("horse", 13));
+
+const funTypes = (jsType) => {
+
 };
 
 const rounder = () => {
