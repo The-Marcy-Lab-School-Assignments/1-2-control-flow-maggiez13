c4bc6de23e9e4e6cd8ea96f5c10a27a11dd6d046
Mei Zhu <maggiez0613@gmail.com>
2024-04-04 13:40:50 -0400

    q10 done

--- src/debug.js
+++ src/debug.js
@@ -44,19 +44,35 @@ const funkoPopAddictionLevel = (numOfFunkoPops) => {
 
 
 const getWeatherReport = (temperature) => {
+  const report = "And that's your report!"
   if (temperature > 90) {
-    const weatherReport = "It's hot and gross out.";
+    let weatherReport = "It's hot and gross out.";
     console.log(weatherReport);
+    console.log(report);
+    return weatherReport;
   } else if (temperature > 70) {
-    const weatherReport = "At least it's a dry heat.";
+    let weatherReport = "At least it's a dry heat.";
     console.log(weatherReport);
+    console.log(report);
+    return weatherReport;
   } else if (temperature < 32) {
-    const weatherReport = "Wow, it's cold out.";
+    let weatherReport = "Wow, it's cold out.";
     console.log(weatherReport);
+    console.log(report);
+    return weatherReport;
+  } else if (temperature === 32) {
+    let weatherReport = "It's not too bad!";
+    console.log(weatherReport);
+    console.log(report);
+    return weatherReport;
   }
-  console.log("And that's your report!");
-  return weatherReport;
 };
+
+getWeatherReport(100);
+getWeatherReport(31);
+getWeatherReport(71);
+getWeatherReport(32);
+
 
 const returnPositiveNegativeZero = (num) => {
   return num < 0
